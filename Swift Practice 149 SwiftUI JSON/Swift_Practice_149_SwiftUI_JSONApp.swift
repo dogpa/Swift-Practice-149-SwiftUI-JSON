d2c32e17514a7690a526b85e712e30dd86830017
Dogpa <dogpa0130@gmail.com>
2022-04-26 21:45:43 +0800

    Initial Commit

---
+++ Swift Practice 149 SwiftUI JSON/Swift_Practice_149_SwiftUI_JSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Practice_149_SwiftUI_JSONApp.swift
+//  Swift Practice 149 SwiftUI JSON
+//
+//  Created by Dogpa's MBAir M1 on 2022/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Practice_149_SwiftUI_JSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
